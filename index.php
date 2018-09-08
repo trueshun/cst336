9b3292cf7a1127602bddaf9230d6cca440853d9f
Zobeyda <zchavez@csumb.edu>
2018-09-08 01:26:17 +0000

    ?

--- index.php
+++ index.php
@@ -1,10 +1,16 @@
-<html><body>
+<!DOCTYPE     HTML>
+
+<html>
+    <HEAD>
+        <TITLE> Zobeyda's site</TITLE>
+    </HEAD>
+<body>
 <?php
 // A simple web site in Cloud9 that runs through Apache
 // Press the 'Run' button on the top to start the web server,
 // then click the URL that is emitted to the Output tab of the console
 
-echo 'Hello world from Cloud9!';
+echo "<h1>Going home<h1>";
 
 ?>
 </body>

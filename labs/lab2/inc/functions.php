4a13d40f6d4068ee3cdc6ed95323cfc7cfde619f
Zobeyda <zchavez@csumb.edu>
2018-09-22 19:04:20 +0000

    lab 2 completed. Added fouth element to slot machine.

--- labs/lab2/inc/functions.php
+++ labs/lab2/inc/functions.php
@@ -10,7 +10,8 @@
                     break;
                 case 2: $totalPoints = 250;
                     break;
-
+                case 3: $totalPoints = 900;
+                    break;
             }
             echo "<h2>You won $totalPoints points!</h2>";
         }else{
@@ -29,6 +30,7 @@ function displaySymbol($random_value, $pos){
             break;
         case 2: $symbol = "cherry";
             break;
+        case 3: $symbol = "grapes";
         }
         echo "<img id = 'reel$pos' src=\"img/$symbol.png\" alt='A $symbol' title='A FUCKING " .ucfirst($symbol).", AHHHH' width ='70'/>";
     }
@@ -44,11 +46,11 @@ function displaySymbol($random_value, $pos){
     
     function play(){
           //create varibales to keep track of what symbol is displayed.
-          
           for($i=1; $i<4; $i++){
-              ${"random_value" . $i } = rand(0,2);
+              ${"random_value" . $i } = rand(0,3);
               displaySymbol(${"random_value" . $i}, $i);
           }
+          displayPoints($random_value1, $random_value2, $random_value3);
           
         //   $random_value1 = rand(0,2);
         //   displaySymbol($random_value1);
@@ -58,4 +60,6 @@ function displaySymbol($random_value, $pos){
         //   displaySymbol($random_value3);
         //   displayPoints($random_value1, $random_value2, $random_value3);
     }
+    
+    
 ?>
--- labs/lab2/index.php
+++ labs/lab2/index.php
@@ -10,16 +10,22 @@
     <body>
         
         <div id= "main">
+            <form>
+                <input type="submit" value="Spin!"/>
+            </form>            
             <?php
                 include 'inc/functions.php';
-                
                 play()
             ?>
-            
-            <form>
-                <input type="submit" value="Spin"/>
-            </form>
-        
+
         </div>
     </body>
+    <footer>
+        <hr>
+        CST 336. 2018&copy; Chavez
+        <br /> <br />
+        <img src="../../img/logo.png" alt="The CSUMB logo. A otter's face with CSUMB beneath his mouth." title="OTTTERR!" class ="center"/>
+       
+        <img src="../../img/buddy_verified.png"  alt ="buddy badge" />
+    </footer>
 </html>

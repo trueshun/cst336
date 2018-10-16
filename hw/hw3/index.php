48a21fef4cc3c9b250133f8c1f064dd1ac9e7f35
Zobeyda <zchavez@csumb.edu>
2018-10-16 06:06:08 +0000

    starting to add css, still need to work on validation

--- hw/hw3/index.php
+++ hw/hw3/index.php
@@ -29,95 +29,113 @@
 <html>
     <head>
         <title>buckle up buckaroo, it's quiz time</title>
+        <link rel="stylesheet" type="text/css" href="css/styles.css" />
     </head>
     <body>
-        <form action = "quiz_result.php" method = "POST" id = "quiz">
-            <!-- question 1 -->
-            <!-- radio type -->
-            <li>
-                <h2>1. CSS stands for: </h2>
-                
-                <div>
-                    <input type = "radio" name = "question-1-answers" id = "question-1-answers-A" value ="A">
-                    <label for = "question-1-answers-A">A. Computer Styled Sections</label>
-                </div>
-                
-                <div>
-                    <input type= "radio" name = "question-1-answers" value="B" >
-                    <label for = "question-1-answers-B">B. Cascading Style Sheets</label>
-                </div>
-                
-                <div>
-                    <input type = "radio" name= "question-1-answers" value ="C">
-                    <label for ="question-1-answers-C">C. Crazy Slip'n Slide</label>
-                </div>
-                
-                <div>
-                    <input type = "radio" name = "question-1-answers" value = "D">
-                    <label for ="question-1-answers-D">D. None of the above</label>
-                </div>
-            </li>
-            
-            <!-- Question 2 -->
-            <li>
-                <h2>2. Are you a: </h2>
-                <div>
-                    <input type ="radio" name = "question-2-answers" value="guy">Guy
+        <div class="container">
+            <h1>Buckle up buckaroo, it's quiz time</h1>
+            <form action = "quiz_result.php" method = "POST" id = "quiz">
+                <!-- question 1 -->
+                <!-- radio type -->
+                <or>
+                    <h2>1. CSS stands for: </h2>
                     
-                </div>
-                <div>
-                    <input type="radio" name = "question-2-answers" value ="chick">Chick
-                </div>
-            </li>    
-            <!-- Question 3 -->
-            <li>
-                <h2>3. Who is Ash's main pokemon?</h2>
-                <div>
-                    <input type = "text" name = "question-3-answers" value = "">
-                </div>
-            </li>
-            <!-- Question 4 -->
-            <li>
-                <h2>4. Which is a starter pokemon from the Kanto region</h2>
-                <div>
-                    <input type ="checkbox" name = "question-4-answers" value ="ony">
-                    <label for = "question-4-answers-char">Onyx</label>
-                </div>
-                <div>
-                    <input type = "checkbox" name = "question-4-answers" value ="abra">
-                    <label for="question-4-answers-bulb">Abra</label>
-                </div>
-                <div>
-                    <input type = "checkbox" name = "question-4-answers" value = "kaka">
-                    <label for ="question-4-answers-squrt">kakashi</label>
-                </div>
-                <div>
-                    <input type = "checkbox" name = "question-4-answers" value = "none">
-                    <label for="question-4-answers-psy">none of the above</label>
-                </div>
-            </li>
-            
-            <li>
-                <h2>5.What's Professor Oak's grandson's name?</h2>
-                <div>
-                    <input type="radio" name = "question-5-answers" value = "A">
-                    <label for="question-5-answers">Loser</label>
-                </div>
-                <div>
-                    <input type="radio" name = "question-5-answers" value = "B">
-                    <label for="question-5-answers">Ass</label>
-                </div>
-                <div>
-                    <input type="radio" name = "question-5-answers" value = "C">
-                    <label for="question-5-answers">Smell ya later</label>
-                </div>
-                <div>
-                    <input type="radio" name = "question-5-answers" value = "D">
-                    <label for="question-5-answers">Gary</label>
-                </div>
-            </li>
-            <br />
-            <input type= "submit" name = "submit" value = "Submit quiz!">
-        </form>
+                    <div>
+                        <input type = "radio" name = "question-1-answers" id = "question-1-answers-A" value ="A">
+                        <label for = "question-1-answers-A">A. Computer Styled Sections</label>
+                    </div>
+                    
+                    <div>
+                        <input type= "radio" name = "question-1-answers" value="B" >
+                        <label for = "question-1-answers-B">B. Cascading Style Sheets</label>
+                    </div>
+                    
+                    <div>
+                        <input type = "radio" name= "question-1-answers" value ="C">
+                        <label for ="question-1-answers-C">C. Crazy Slip'n Slide</label>
+                    </div>
+                    
+                    <div>
+                        <input type = "radio" name = "question-1-answers" value = "D">
+                        <label for ="question-1-answers-D">D. None of the above</label>
+                    </div>
+                </or>
+                
+                <!-- Question 2 -->
+                <or>
+                    <h2>2. Are you a: </h2>
+                    <div>
+                        <input type ="radio" name = "question-2-answers" value="guy">
+                        <label for ="question-2-answers-guy">Guy</label>
+                    </div>
+                    <div>
+                        <input type="radio" name = "question-2-answers" value ="chick">
+                        <label for="question-2-answers-chick">Chick</label>
+                    </div>
+                </or>    
+                <!-- Question 3 -->
+                <or>
+                    <h2>3. Who is Ash's main pokemon?</h2>
+                    <div>
+                        <input type = "text" name = "question-3-answers" value = "">
+                    </div>
+                </or>
+                <!-- Question 4 -->
+                <or>
+                    <h2>4. Which is a starter pokemon from the Kanto region</h2>
+                    <div>
+                        <input type ="checkbox" name = "question-4-answers" value ="ony">
+                        <label for = "question-4-answers-char">Onyx</label>
+                    </div>
+                    <div>
+                        <input type = "checkbox" name = "question-4-answers" value ="abra">
+                        <label for="question-4-answers-bulb">Abra</label>
+                    </div>
+                    <div>
+                        <input type = "checkbox" name = "question-4-answers" value = "kaka">
+                        <label for ="question-4-answers-squrt">kakashi</label>
+                    </div>
+                    <div>
+                        <input type = "checkbox" name = "question-4-answers" value = "none">
+                        <label for="question-4-answers-psy">none of the above</label>
+                    </div>
+                </or>
+                <!-- Question 5--> 
+                <or>
+                    <h2>5.What's Professor Oak's grandson's name?</h2>
+                    <div>
+                        <input type="radio" name = "question-5-answers" value = "A">
+                        <label for="question-5-answers">Loser</label>
+                    </div>
+                    <div>
+                        <input type="radio" name = "question-5-answers" value = "B">
+                        <label for="question-5-answers">Ass</label>
+                    </div>
+                    <div>
+                        <input type="radio" name = "question-5-answers" value = "C">
+                        <label for="question-5-answers">Smell ya later</label>
+                    </div>
+                    <div>
+                        <input type="radio" name = "question-5-answers" value = "D">
+                        <label for="question-5-answers">Gary</label>
+                    </div>
+                </or>
+                
+                <!-- Question 6 -->
+                <or>
+                    <h2>6. Bye, bye </h2>
+                    <div>
+                        <select name = "question-6-answers">
+                            <option value = "a">Pikachu!</option>
+                            <option value ="b">life!</option>
+                            <option value ="c">Butterfree!</option>
+                            <option value ="d">Misty!</option>
+                        </select>
+                    </div>
+                </or>
+                <br />
+                <input type= "submit" name = "submit" value = "Submit quiz!">
+            </form>
+        </div>
     </body>
 </html>
--- hw/hw3/quiz_result.php
+++ hw/hw3/quiz_result.php
@@ -4,14 +4,19 @@
     $answer3 = $_POST['question-3-answers'];
     $answer4 = $_POST['question-4-answers'];
     $answer5 = $_POST['question-5-answers'];
+    $answer6 = $_POST['question-6-answers'];
     
     $totalCorrect = 0;
+    $congrats = "";
     
     if($answer1 == "B") {$totalCorrect++;}
     if($answer2 == "guy") {$gender = "you're a dude";}else{$gender = "you're a dudette";}
     if($answer3 == "pikachu" || $answer3 == "Pikachu") {$totalCorrect++;}
     if($answer4 == "none") {$totalCorrect++;}
     if($answer5 == "D") {$totalCorrect++;}
+    if($answer6 == "c"){$totalCorrect++;}
     
-    echo "<div id='results'>$totalCorrect / 4 correct, and $gender</div>";
+    if($totalCorrect == 5){$congrats = "Yay, ya got them all! Doing way better than, Ash!";}
+    
+    echo "<div id='results'>$totalCorrect / 5 correct, and $gender. $congrats </div>";
 ?>
